97990e5531b4c6b84a6131e0d5c1291dfbfce809
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-11-30 21:22:41 +0600

    User schema created

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,55 @@
+import { Schema, model, connect } from "mongoose";
+import { TAddress, TOrders, TUsers } from "./users.interface";
+
+const addressSchema = new Schema<TAddress>({
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+});
+const ordersSchema = new Schema<TOrders>({
+  productName: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+const usersSchema = new Schema<TUsers>({
+  userId: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  hobbies: [{ type: String }],
+  address: {
+    type: addressSchema,
+    required: true,
+  },
+  orders: [ordersSchema],
+});
