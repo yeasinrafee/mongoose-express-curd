9098c39671c0e13b10b790c0fc69222f0f2fc827
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-11-30 21:05:26 +0600

    users interface created

---
+++ src/app/modules/users/users.interface.ts
@@ -0,0 +1,25 @@
+export type TAddress = {
+  street: string;
+  city: string;
+  country: string;
+};
+export type TOrders = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+export type TUsers = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: Array<string>;
+  address: TAddress;
+  orders?: TOrders[];
+};
